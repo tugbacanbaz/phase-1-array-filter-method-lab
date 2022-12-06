0495bc0f089c34dd4b5c895b4fc72e0ec3325bba
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-06 18:29:53 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+const findMatching = (drivers, name) => {
+
+  return drivers.filter(
+    (driver) => (driver = name && driver.toUpperCase() === name.toUpperCase())
+
+  );
+};
+
+
+
+const fuzzyMatch = (drivers, name) => {
+    return drivers.filter((driver) => driver.startsWith(name));
+  };
+
+
+
+  function matchName(drivers, name){
+    const matchNames = drivers.filter(function(item){
+        if(item.name === name){
+            return item
+        }
+    })
+    return matchNames
+}
